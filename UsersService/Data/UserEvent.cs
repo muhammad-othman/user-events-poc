a858ca09049914cf205482af4dbcddb5086ab6b5
Muhammad Othman <muhammad.oth91@gmail.com>
2023-06-03 08:04:04 -0400

    Send user events from usersService

---
+++ UsersService/Data/UserEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace UsersService.Data
+{
+    public class UserEvent
+    {
+        public EventType Type { get; set; }
+    }
+
+    public enum EventType
+    {
+        Created = 0,
+        Updated = 1,
+        Deleted = 2,
+    }
+}
--- UsersService/Data/UserRepository.cs
+++ UsersService/Data/UserRepository.cs
@@ -1,19 +1,42 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
 
 namespace UsersService.Data
 {
     public class UserRepository : IUserRepository
     {
         private readonly ApplicationDBContext context;
+        private readonly IModel rabbitmqModel;
 
         public UserRepository(ApplicationDBContext context)
         {
             this.context = context;
+
+            var factory = new ConnectionFactory();
+
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            factory.HostName = "45.63.116.153";
+            factory.Port = 5672;
+
+            var connection = factory.CreateConnection();
+
+            rabbitmqModel = connection.CreateModel();
+            rabbitmqModel.ExchangeDeclare("usersbus", ExchangeType.Fanout, true);
+
         }
         public async Task<User> Add(User user)
         {
             context.Add<User>(user);
             await context.SaveChangesAsync();
+
+            var userEvent = new UserEvent
+            {
+                Type = EventType.Created,
+            };
+            PublishEvent(userEvent);
             return user;
         }
 
@@ -26,6 +49,12 @@ namespace UsersService.Data
 
             context.Users.Remove(userToDelete);
             await context.SaveChangesAsync();
+
+            var userEvent = new UserEvent
+            {
+                Type = EventType.Deleted,
+            };
+            PublishEvent(userEvent);
         }
 
         public async Task<List<User>> GetAll()
@@ -46,7 +75,24 @@ namespace UsersService.Data
 
             await context.SaveChangesAsync();
 
+            var userEvent = new UserEvent
+            {
+                Type = EventType.Updated,
+            };
+            PublishEvent(userEvent);
+
             return userToUpdate;
+        }
+
+        private void PublishEvent(UserEvent userEvent) 
+        {
+            var jsonEvent = JsonSerializer.Serialize(userEvent, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            });
+            var body = Encoding.UTF8.GetBytes(jsonEvent);
+
+            rabbitmqModel.BasicPublish("usersbus", "", null, body);
         }
     }
 }
